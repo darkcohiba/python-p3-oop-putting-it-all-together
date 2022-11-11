bfe55ad83eee82b25ec0648e3723f4e29be90a27
Sam <samuel.waters@gmail.com>
2022-11-10 22:49:12 -0700

    finished

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.author = ""
+        self.page_count = 0
+        self.genre = ""
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+# FAILED Book in book.py gets initialized with a title. - TypeError: Book() takes no arguments
+# FAILED Book in book.py has the title passed into __init__. - TypeError: Book() takes no arguments
+# FAILED Book in book.py can be assigned an author name. - TypeError: Book() takes no arguments
+# FAILED Book in book.py can be assigned a page count. - TypeError: Book() takes no arguments
+# FAILED Book in book.py can be assigned a genre. - TypeError: Book() takes no arguments
+# FAILED Book in book.py outputs "Flipping the page...wow, you read fast!" when method turn_page() is called - TypeError: Book() takes no arguments
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+        self.color = ""
+        self.size = 0
+        self.material = ""
+        self.condition = "old"
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+    
+
+
+# FAILED Shoe in shoe.py gets initialized with a brand. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py has the brand passed to __init__. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a color. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a size. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a material. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py can be assigned a condition. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py says that the shoe has been repaired. - TypeError: Shoe() takes no arguments
+# FAILED Shoe in shoe.py has "New" condition after repair. - TypeError: Shoe() takes no arguments
